277adab229d4acc14bf9088e6eeefadaba864a83
George Bennett (oolong-wsl2) <george.bennett@memphistn.gov>
2026-03-08 13:44:24 -0500

    improve /scripts/update_index_version.sh to handle README.md helptree versioning in addition to index

--- scripts/update_index_version.sh
+++ scripts/update_index_version.sh
@@ -3,6 +3,7 @@ set -e
 
 # --- CONFIG ---
 INDEX_FILE="docs/index.md"
+README_FILE="README.md"
 SVG_DIR="assets"
 BASE_SVG_NAME="dworshak_v"
 CLI_CMD="dworshak"
@@ -11,12 +12,16 @@ CLI_CMD="dworshak"
 VERSION=$($CLI_CMD --version | awk '{print $NF}')
 echo "Detected Dworshak CLI version: $VERSION"
 
-# --- Build new SVG filename ---
+# --- SVG filename based on CLI version ---
 NEW_SVG="${SVG_DIR}/${BASE_SVG_NAME}${VERSION}_helptree.svg"
 echo "New helptree SVG: $NEW_SVG"
 
 # --- Replace old SVG reference in index.md ---
 # Match any old dworshak_v<version>_helptree.svg
 sed -i -E "s|${BASE_SVG_NAME}[0-9]+\.[0-9]+\.[0-9]+_helptree\.svg|${BASE_SVG_NAME}${VERSION}_helptree.svg|g" "$INDEX_FILE"
-
 echo "Updated $INDEX_FILE to use version $VERSION"
+
+# --- Replace old SVG reference in README.md ---
+# Match any old dworshak_v<version>_helptree.svg
+sed -i -E "s|${BASE_SVG_NAME}[0-9]+\.[0-9]+\.[0-9]+_helptree\.svg|${BASE_SVG_NAME}${VERSION}_helptree.svg|g" "$README_FILE"
+echo "Updated $README_FILE to use version $VERSION"
